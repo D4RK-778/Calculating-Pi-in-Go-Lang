3422113a1c0a564bcb33a61da0bf660142b3b928
D4RK778_Logic <thalisbrumreall@gmail.com>
2026-02-17 14:51:39 +0000

    Update calculate function and output format
    Refactor the main function and calculation output.

--- Pin.go
+++ Pin.go
@@ -1,19 +1,27 @@
 package main
 
+// import
 import (
 	"fmt"
 	"math"
 )
 
+// variables
 var diameter float64 = 31.83
-var perimeter float64 = 100
+var perimeter float64 = 100.0
 
-func main() {
+// function to calculate
+func calculate(diameter float64, perimeter float64) {
 	cal := perimeter / diameter
 	real := math.Pi
 	diference := math.Abs(real - cal)
 
-	fmt.Println(cal)
-	fmt.Println(real)
-	fmt.Println(diference)
+	fmt.Println(cal, real, diference)
 }
+
+// main function
+func main() {
+	calculate(diameter, perimeter)
+}
+
+// Go Lang is a cool language
